d004eabf3108251357c1412bf8a7a068d386d0c3
CauaZaneteSouza <158862182+CauaZaneteSouza@users.noreply.github.com>
2024-08-13 11:55:07 -0300

    Jogo da velha trabalho

---
+++ Trabalho_jogo_velha/Trabalho_jogo_velha/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Trabalho_jogo_velha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lbl_info = new System.Windows.Forms.Label();
+            this.btn_01 = new System.Windows.Forms.Button();
+            this.btn_06 = new System.Windows.Forms.Button();
+            this.btn_05 = new System.Windows.Forms.Button();
+            this.btn_04 = new System.Windows.Forms.Button();
+            this.btn_03 = new System.Windows.Forms.Button();
+            this.btn_02 = new System.Windows.Forms.Button();
+            this.btn_09 = new System.Windows.Forms.Button();
+            this.btn_08 = new System.Windows.Forms.Button();
+            this.btn_07 = new System.Windows.Forms.Button();
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_info
+            // 
+            this.lbl_info.AutoSize = true;
+            this.lbl_info.Location = new System.Drawing.Point(40, 117);
+            this.lbl_info.Name = "lbl_info";
+            this.lbl_info.Size = new System.Drawing.Size(76, 13);
+            this.lbl_info.TabIndex = 16;
+            this.lbl_info.Text = "O, é a sua vez";
+            // 
+            // btn_01
+            // 
+            this.btn_01.Location = new System.Drawing.Point(12, 12);
+            this.btn_01.Name = "btn_01";
+            this.btn_01.Size = new System.Drawing.Size(41, 23);
+            this.btn_01.TabIndex = 17;
+            this.btn_01.UseVisualStyleBackColor = true;
+            // 
+            // btn_06
+            // 
+            this.btn_06.Location = new System.Drawing.Point(106, 41);
+            this.btn_06.Name = "btn_06";
+            this.btn_06.Size = new System.Drawing.Size(41, 23);
+            this.btn_06.TabIndex = 18;
+            this.btn_06.UseVisualStyleBackColor = true;
+            // 
+            // btn_05
+            // 
+            this.btn_05.Location = new System.Drawing.Point(59, 41);
+            this.btn_05.Name = "btn_05";
+            this.btn_05.Size = new System.Drawing.Size(41, 23);
+            this.btn_05.TabIndex = 19;
+            this.btn_05.UseVisualStyleBackColor = true;
+            // 
+            // btn_04
+            // 
+            this.btn_04.Location = new System.Drawing.Point(12, 41);
+            this.btn_04.Name = "btn_04";
+            this.btn_04.Size = new System.Drawing.Size(41, 23);
+            this.btn_04.TabIndex = 20;
+            this.btn_04.UseVisualStyleBackColor = true;
+            // 
+            // btn_03
+            // 
+            this.btn_03.Location = new System.Drawing.Point(106, 12);
+            this.btn_03.Name = "btn_03";
+            this.btn_03.Size = new System.Drawing.Size(41, 23);
+            this.btn_03.TabIndex = 21;
+            this.btn_03.UseVisualStyleBackColor = true;
+            // 
+            // btn_02
+            // 
+            this.btn_02.Location = new System.Drawing.Point(59, 12);
+            this.btn_02.Name = "btn_02";
+            this.btn_02.Size = new System.Drawing.Size(41, 23);
+            this.btn_02.TabIndex = 22;
+            this.btn_02.UseVisualStyleBackColor = true;
+            // 
+            // btn_09
+            // 
+            this.btn_09.Location = new System.Drawing.Point(106, 70);
+            this.btn_09.Name = "btn_09";
+            this.btn_09.Size = new System.Drawing.Size(41, 23);
+            this.btn_09.TabIndex = 23;
+            this.btn_09.UseVisualStyleBackColor = true;
+            // 
+            // btn_08
+            // 
+            this.btn_08.Location = new System.Drawing.Point(59, 70);
+            this.btn_08.Name = "btn_08";
+            this.btn_08.Size = new System.Drawing.Size(41, 23);
+            this.btn_08.TabIndex = 24;
+            this.btn_08.UseVisualStyleBackColor = true;
+            // 
+            // btn_07
+            // 
+            this.btn_07.Location = new System.Drawing.Point(12, 70);
+            this.btn_07.Name = "btn_07";
+            this.btn_07.Size = new System.Drawing.Size(41, 23);
+            this.btn_07.TabIndex = 25;
+            this.btn_07.UseVisualStyleBackColor = true;
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.AutoSize = true;
+            this.lbl_result.Location = new System.Drawing.Point(43, 159);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(0, 13);
+            this.lbl_result.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(163, 201);
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.btn_07);
+            this.Controls.Add(this.btn_08);
+            this.Controls.Add(this.btn_09);
+            this.Controls.Add(this.btn_02);
+            this.Controls.Add(this.btn_03);
+            this.Controls.Add(this.btn_04);
+            this.Controls.Add(this.btn_05);
+            this.Controls.Add(this.btn_06);
+            this.Controls.Add(this.btn_01);
+            this.Controls.Add(this.lbl_info);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lbl_info;
+        private System.Windows.Forms.Button btn_01;
+        private System.Windows.Forms.Button btn_06;
+        private System.Windows.Forms.Button btn_05;
+        private System.Windows.Forms.Button btn_04;
+        private System.Windows.Forms.Button btn_03;
+        private System.Windows.Forms.Button btn_02;
+        private System.Windows.Forms.Button btn_09;
+        private System.Windows.Forms.Button btn_08;
+        private System.Windows.Forms.Button btn_07;
+        private System.Windows.Forms.Label lbl_result;
+    }
+}
+
---
+++ Trabalho_jogo_velha/Trabalho_jogo_velha/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabalho_jogo_velha
+{
+    public partial class Form1 : Form
+    {
+
+        bool x = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_load(object senter, EventArgs e)
+        {
+            btn_01.Click += new EventHandler(BClick);
+            btn_02.Click += new EventHandler(BClick);
+            btn_03.Click += new EventHandler(BClick);
+            btn_04.Click += new EventHandler(BClick);
+            btn_05.Click += new EventHandler(BClick);
+            btn_06.Click += new EventHandler(BClick);
+            btn_07.Click += new EventHandler(BClick);
+            btn_08.Click += new EventHandler(BClick);
+            btn_09.Click += new EventHandler(BClick);
+
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    item.TabStop = false;
+                }
+            }
+        }
+        private void BClick(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.x ? "O" : "X";
+            ((Button)sender).Enabled = false;
+            Verificar_ganhador();
+            x = !x;
+            lbl_info.Text = string.Format("{0}, é a sua vez", this.x ? "O" : "X");
+        }
+        private void Verificar_ganhador()
+        {
+            if (btn_01.Text != " " && btn_01.Text == btn_02.Text && btn_02.Text == btn_03.Text || //linha 1
+                btn_04.Text != " " && btn_04.Text == btn_05.Text && btn_05.Text == btn_06.Text || //linha 2
+                btn_07.Text != " " && btn_07.Text == btn_08.Text && btn_08.Text == btn_09.Text || //linha 3
+
+                btn_01.Text != " " && btn_01.Text == btn_04.Text && btn_08.Text == btn_07.Text || //coluna 1
+                btn_02.Text != " " && btn_02.Text == btn_05.Text && btn_05.Text == btn_08.Text || //coluna 2
+                btn_03.Text != " " && btn_03.Text == btn_06.Text && btn_06.Text == btn_09.Text || //coluna 3
+
+                btn_01.Text != " " && btn_01.Text == btn_05.Text && btn_05.Text == btn_09.Text || // diagonal principal
+                btn_03.Text != " " && btn_03.Text == btn_05.Text && btn_05.Text == btn_07.Text || //diagonal secundária
+              )
+
+            { MessageBox.Show(string.Format(" O ganhador é o [{0}]", x ? "O" : "X"), "Temos o ganhador",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Reiniciar();
+            }
+            else
+            {
+                Verificar_empate();
+            }
+        }
+        private void Verificar_empate() {
+            bool tudo_inativo = true;
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button && item.Enabled)
+                {
+                    tudo_inativo = false;
+                    break;
+                }
+            }
+            if (tudo_inativo)
+            {
+                MessageBox.Show(string.Format("Empatou"), "wow",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Reiniciar();
+            }
+        }
+        private void Reiniciar() {
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    item.Enabled = true;
+                    item.Text = " ";
+                }
+            }
+        }
+    } 
+}
